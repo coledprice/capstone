17299ce8a82fd7d22f568c1716ecb20e318fd84b
Cole Price <colevart@gmail.com>
2023-01-02 15:14:59 -0500

    creates login

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get "/fishs/:id" => "fishs#show"
   post "/fishs" => "fishs#create"
   post "/users" => "users#create"
+  post "/sessions" => "sessions#create"
 end
